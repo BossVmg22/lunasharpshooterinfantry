9bb2bfa319a35bc52acad01a58e19136dcd55479
BossVmg22 <vincentplayerzfx101@gmail.com>
2026-03-10 00:51:48 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://srtwguxztrgolf..."  // your actual URL
-const supabaseKey = "eyJhbGciOiJl..."           // your actual key
+const supabaseUrl = "https://srtwguxztrgolfwihcsp.supabase.co"  // your actual URL
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydHdndXh6dHJnb2xmd2loY3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODg3MjAsImV4cCI6MjA4ODU2NDcyMH0.AR8Zda3RXO7hd-V-GqRuaWqg6C9ZYU20qJsfW8oV_c4"           // your actual key
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
